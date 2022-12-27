ff7d01c28852af17e2786b8bed38444b1af414f9
Zach Angold <zangold@fastmail.com>
2022-12-26 20:38:25 -0500

    solve maximum-bags-with-full-capacity-of-rocks

---
+++ maximum-bags-with-full-capacity-of-rocks/src/lib.rs
@@ -0,0 +1,51 @@
+#![allow(dead_code)]
+
+struct Solution;
+
+impl Solution {
+    pub fn maximum_bags(mut capacity: Vec<i32>, rocks: Vec<i32>, mut additional_rocks: i32) -> i32 {
+        capacity
+            .iter_mut()
+            .zip(rocks.iter())
+            .for_each(|(cap, rocks)| *cap -= rocks);
+        capacity.sort_unstable();
+
+        let mut filled = 0;
+        for c in capacity {
+            if additional_rocks < c {
+                break;
+            }
+
+            additional_rocks -= c;
+            filled += 1;
+        }
+
+        filled
+    }
+}
+
+#[test]
+fn test1() {
+    let capacity = vec![2, 3, 4, 5];
+    let rocks = vec![1, 2, 4, 4];
+    let additional_rocks = 2;
+    let expected = 3;
+
+    assert_eq!(
+        Solution::maximum_bags(capacity, rocks, additional_rocks),
+        expected
+    );
+}
+
+#[test]
+fn test2() {
+    let capacity = vec![10, 2, 2];
+    let rocks = vec![2, 2, 0];
+    let additional_rocks = 100;
+    let expected = 3;
+
+    assert_eq!(
+        Solution::maximum_bags(capacity, rocks, additional_rocks),
+        expected
+    );
+}
