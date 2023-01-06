2c3620014b6952cd3af65170dc54a14c2794f33c
Zach Angold <zangold@fastmail.com>
2023-01-06 18:47:06 -0500

    Solve power-of-three

---
+++ power-of-three/src/lib.rs
@@ -0,0 +1,23 @@
+#![allow(dead_code)]
+
+struct Solution;
+
+impl Solution {
+    pub const fn is_power_of_three(mut n: i32) -> bool {
+        while n > 1 && n % 3 == 0 {
+            n /= 3;
+        }
+
+        n == 1
+    }
+}
+
+#[test]
+fn do_test() {
+    assert!(Solution::is_power_of_three(27));
+    assert!(Solution::is_power_of_three(81));
+    assert!(!Solution::is_power_of_three(14));
+    assert!(!Solution::is_power_of_three(72));
+    assert!(!Solution::is_power_of_three(0));
+    assert!(!Solution::is_power_of_three(-1));
+}
