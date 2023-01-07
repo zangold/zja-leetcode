f4e99a9f932577a5e13e439995ca82559d9aac3e
Zach Angold <zangold@fastmail.com>
2023-01-07 12:52:10 -0500

    solve missing-number

---
+++ missing-number/src/lib.rs
@@ -0,0 +1,16 @@
+#![allow(dead_code)]
+
+struct Solution;
+
+impl Solution {
+    pub fn missing_number(nums: Vec<i32>) -> i32 {
+        (nums.len() * (nums.len() + 1) / 2) as i32 - nums.into_iter().sum::<i32>()
+    }
+}
+
+#[test]
+fn do_test() {
+    assert_eq!(Solution::missing_number(vec![3, 0, 1]), 2);
+    assert_eq!(Solution::missing_number(vec![0, 1]), 2);
+    assert_eq!(Solution::missing_number(vec![9, 6, 4, 2, 3, 5, 7, 0, 1]), 8);
+}
